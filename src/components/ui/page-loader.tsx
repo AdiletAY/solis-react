4ea9d7fea05b033e870f7911aac8a8bd466dd555
Ruslan <cooperyan66@gmail.com>
2024-11-03 01:15:06 +0500

    styled loader

--- src/components/ui/page-loader.tsx
+++ src/components/ui/page-loader.tsx
@@ -1,11 +1,12 @@
-import { LoaderIcon } from 'lucide-react';
-import { Typography } from './typography';
+import '@/styles/loader.css';
+import LogotypeMin from "@/components/ui/logotype-min.tsx";
 
 const PageLoader = () => {
     return (
         <div className="h-screen flex flex-col justify-center items-center">
-            <LoaderIcon className="w-10 h-10 animate-spin ease-in-out duration-1000" />
-            <Typography tag="h2">loading...</Typography>
+          <div className='animate-logo-loader'>
+            <LogotypeMin/>
+          </div>
         </div>
     );
 };
